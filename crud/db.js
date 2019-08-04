fb9cde2bb28bf6902dfcdecde095564c639f34c6
Murilo Portugal <murilo.portugal@gmail.com>
2019-08-04 20:14:53 -0300

    Finalizado CRUD

--- crud/db.js
+++ crud/db.js
@@ -8,4 +8,26 @@ function findAll(callback){
     global.conn.collection("customers").find({}).toArray(callback);
 }
 
-module.exports = {findAll}
+function insert(customer,callback){
+    global.conn.collection("customers").insert(customer,callback);
+}
+//O id vindo da página vem como String, por isso devemos converter em ObjectID do mongodb
+const objectId = require("mongodb").ObjectID;
+function findOne(id,callback){
+    global.conn.collection("customers").findOne(
+        new objectId(id),
+        callback);
+}
+
+function update(id,customer,callback){
+    global.conn.collection("customers").updateOne(
+        {_id:new objectId(id)},
+        {$set:customer},
+        callback);
+}
+
+function deleteOne(id,callback){
+    global.conn.collection("customers").deleteOne({_id:new objectId(id)},callback);
+}
+
+module.exports = {findAll,insert,findOne,update,deleteOne}
--- crud/routes/index.js
+++ crud/routes/index.js
@@ -9,12 +9,53 @@ router.get('/', function(req, res, next) {
   });
 });
 
+/* GET pagina de cadastro */
 router.get('/new',function(req,res,next) {
-  res.render('new',{title:"Cadastro de Cliente",action:"/new"});  
+  res.render('new',{title:"Cadastro de Cliente",doc:{},action:"/new"});
 });
 
+/* Post cadastra novo cliente */
 router.post('/new',function(req,res,next){
-  res.redirect('/?new=true');
+  const nome  = req.body.nome;
+  const idade = parseInt(req.body.idade);
+  const uf    = req.body.uf;
+  global.db.insert({nome,idade,uf},(err,result)=>{
+    if(err){return console.log(err);}
+    res.redirect('/?new=true');
+  });
+});
+
+/* GET Editar cliente */
+router.get('/edit/:id',function(req,res,next){
+  var id = req.params.id;
+  global.db.findOne(id,(e,doc)=>{
+    if(e){return console.log(e);}
+    //console.log(doc.nome);
+    res.render('new',{title:'Edição de Cliente',doc:doc,action:'/edit/'+doc._id});
+  });
+});
+
+/* POST Update dados do cliente */
+router.post('/edit/:id',function(req,res,next){
+  const id = req.params.id;
+  const nome = req.body.nome;
+  const idade = parseInt(req.body.idade);
+  const uf = req.body.uf;
+
+  global.db.update(id,{nome,idade,uf},(e,result)=>{
+    if(e){return console.log(e);}
+    res.redirect('/?edit=true');
+  });
+
+});
+
+/* GET Delete Cliente */
+router.get('/delete/:id',function(req,res){
+  var id = req.params.id;
+  global.db.deleteOne(id,(e,result)=>{
+    if(e){return console.log(e);}
+    res.redirect('/?delete=true');
+  });
 });
 
 module.exports = router;
